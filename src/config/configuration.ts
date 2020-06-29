94d392b043639f482d824933ec37d47978dd4fdb
NghiepNguyen <asta@Nghieps-MacBook-Pro.local>
2020-06-29 15:47:44 +0700

    [HotFix] edit .env

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,5 +1,5 @@
 export const Config = {
-    API_REFERRAL_SERVICE: process.env.REFFERAL_API_URL || 'http://localhost:8000',
+    API_REFERRAL_SERVICE: process.env.REFFERAL_API_URL || 'https://api-referral.vndigitech.com',
     EMAIL_CLIENT: process.env.REFFERAL_API_EMAIL ||  'client@gmail.com',
     PASSWORD_CLIENT: process.env.REFFERAL_API_PASSWORD  || '12345678',
 }
